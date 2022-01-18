711466fa5ff9c3915b632f3169c0bb4bb2f69bb3
Rohit Sangwan <rohitsangwan@Rohits-MacBook-Air.local>
2022-01-18 00:01:25 -0800

    null safr

---
+++ example/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:document_file_save/document_file_save.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    List<int> htmlBytes = utf8.encode("<h1>Header 1</h1><p>This is sample text</p>");
+    List<int> textBytes = utf8.encode("Some data");
+
+    // save multiple files
+    DocumentFileSave.saveMultipleFiles([htmlBytes, textBytes], ["htmlfile.html", "textfile.txt"], ["text/html", "text/plain"]);
+
+    // save multiple files (case that file have same name). system will automatically append number to filename.
+    //DocumentFileSave.saveMultipleFiles([htmlBytes, textBytes], ["file.txt", "file.txt"], ["text/html", "text/plain"]);
+
+    // save single file
+    // DocumentFileSave.saveFile(htmlBytes, "my test html file.html", "text/html");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('document_file_save Plugin'),
+        ),
+        body: Center(
+          child: Text('Please check file in Download folder (or Files App in iOS)'),
+        ),
+      ),
+    );
+  }
+
+}
